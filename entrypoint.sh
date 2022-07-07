f89ca1cd825858e067a555ad0ad39bfe0d0ff305
Connor Bracy <cjb2250@columbia.edu>
2022-07-07 10:48:44 -0400

    Commenting scratch/example lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,14 +92,14 @@ else
   echo "No changes detected"
 fi
 ################################################################################
-if git status | grep -q "Changes to be committed"
-then
-  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-  echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
-               -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS
+# if git status | grep -q "Changes to be committed"
+# then
+#   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+#   echo "Pushing git commit"
+#   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+#   echo "Creating a pull request"
+#   gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+#                -b $INPUT_DESTINATION_HEAD_BRANCH \
+#                -B $INPUT_DESTINATION_BASE_BRANCH \
+#                -H $INPUT_DESTINATION_HEAD_BRANCH \
+#                   $PULL_REQUEST_REVIEWERS
